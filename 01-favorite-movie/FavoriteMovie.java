3b4d024f1b0fa38b7a51361c572bc915467bc9de
AP Java Period 5 <apjavaperiod5@ac1mwk158jc9h.78b505.nycboe.org>
2022-10-04 11:50:24 -0400

    Added FavoriteMovie files

---
+++ 01-favorite-movie/FavoriteMovie.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class FavoriteMovie here.
+ *
+ * @author Lisa Ng
+ * @09/21/22
+ */
+public class FavoriteMovie{
+public static void main(String[] args) {
+  System.out.println("My favorite movie is called Level 16.");
+    
+  System.out.println("It's about a group of girls who lives in a boarding school where they are taught to be perfect girls by being obedient and pure.");
+    
+  System.out.println("These girls are told that they would one day be adopted by rich families when they reach a certain age."); 
+    
+  System.out.println("Every year, the girls move up a level until they reach the highest level which is level 16.");
+
+  System.out.println("However, there is a dark secret about why these girls are being held captive in this facility. This led to two girls named Vivien and Sophia attempts to escape after learning the bitter truth about their captivity.");
+   }
+}
